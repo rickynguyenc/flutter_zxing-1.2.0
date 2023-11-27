278d3a55a09ecb7cc93f47f316cb594125b17f9b
chuongnv <chuongnv@dft.vn>
2023-11-27 17:06:57 +0700

    kkkk

---
+++ example/macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=C:\Users\chuon\Documents\flutter"
+export "FLUTTER_APPLICATION_PATH=D:\Work\RFID_VNA\DEMO\flutter_zxing-1.2.0\example"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=0.0.1"
+export "FLUTTER_BUILD_NUMBER=0.0.1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
