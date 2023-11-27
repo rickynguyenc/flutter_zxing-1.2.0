30776daf60e012d81ae9c1ee4408fcc1157ecfc3
chuongnv <chuongnv@dft.vn>
2023-11-27 17:45:46 +0700

    tttttttt

--- src/native_zxing.cpp
+++ src/native_zxing.cpp
@@ -15,8 +15,8 @@ using namespace std;
 extern "C"
 {
     void resultToCodeResult(struct CodeResult *code, Result result)
-    {   
-        platform_log('resut pre convert=========');
+    {
+        std::cout << 'Somehign ==================' << result.text() << std::endl;
         string text = result.text();
         code->text = new char[text.length() + 1];
         strcpy(code->text, text.c_str());
@@ -69,7 +69,7 @@ extern "C"
         }
         DecodeHints hints = DecodeHints().setTryHarder(tryHarder).setTryRotate(tryRotate).setFormats(BarcodeFormat(format)).setTryInvert(tryInvert).setReturnErrors(true);
         Result result = ReadBarcode(image, hints);
-        
+
         delete[] bytes;
 
         struct CodeResult code;
@@ -80,8 +80,6 @@ extern "C"
         code.pos->imageWidth = width;
         code.pos->imageHeight = height;
         platform_log("Read Barcode in: %d ms\n", code.duration);
-        platform_log("abcde ==================================");
-        platform_log("code texxt ================================== %s",code.text);
         return code;
     }
 
